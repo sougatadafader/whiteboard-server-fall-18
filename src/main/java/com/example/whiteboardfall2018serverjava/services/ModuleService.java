d4b47979c72ae466e68d4f973c78c70573c88062
sougatadafader <sougatadafader@gmail.com>
2018-10-21 04:35:20 -0400

    Module Service Completed

--- src/main/java/com/example/whiteboardfall2018serverjava/services/ModuleService.java
+++ src/main/java/com/example/whiteboardfall2018serverjava/services/ModuleService.java
@@ -2,14 +2,17 @@ package com.example.whiteboardfall2018serverjava.services;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,12 +26,11 @@ public class ModuleService {
 	@Autowired
 	CourseService courseService;
 	private List<Module> modules = new ArrayList<Module>();
+
 	@GetMapping("/api/course/{courseId}/module")
-	public List<Module> findModulesForCourse(
-			@PathVariable("courseId") int courseId) {
+	public List<Module> findModulesForCourse(@PathVariable("courseId") int courseId) {
 		Course course = courseService.findCourseById(courseId);
-		if(modules.isEmpty())
-		{
+		if (modules.isEmpty()) {
 			Module m1 = new Module(123, "Module 1");
 			Module m2 = new Module(234, "Module 2");
 			modules.add(m1);
@@ -36,19 +38,85 @@ public class ModuleService {
 			course.setModules(modules);
 			return modules;
 		}
-			
-		modules.addAll(course.getModules());
-		
+
+		modules = course.getModules();
+
 		return modules;
 	}
-	
+
 	@PostMapping("/api/course/{courseId}/module")
 	public List<Module> createModule(@RequestBody Module module, @PathVariable("courseId") int courseId) {
 		Course course = courseService.findCourseById(courseId);
 		modules = course.getModules();
-		int uId= (int)System.currentTimeMillis();
+		int uId = (int) System.currentTimeMillis();
 		module.setId(uId);
 		modules.add(module);
 		return modules;
+	}
+
+	/* Retrieves Module whose id is mid */
+	@GetMapping("/api/module/{mid}")
+	public Module findModuleById(@PathVariable("mid") int mid, HttpSession session) {
+		List<Course> courses = courseService.findAllCourses(session);
+		Module reqdModule = null;
+		for (Course c : courses) {
+			modules = c.getModules();
+			for (Module m : modules) {
+				if (m.getId() == mid) {
+					reqdModule = m;
+					break;
+				}
+			}
+		}
+		return reqdModule;
+	}
+
+	/* Updates Module whose id is mid */
+	@PutMapping("/api/module/{mid}")
+	public Module updateCourse(@PathVariable("mid") int mid, @RequestBody Module module, HttpSession session) {
+
+		List<Course> courses = courseService.findAllCourses(session);
+		int ctr = 0;
+		module.setId(mid);
+		for (Course c : courses) {
+			modules = c.getModules();
+			for (Module m : modules) {
+				if (m.getId() == mid) {
+					modules.set(ctr, module);
+					break;
+				}
+				ctr++;
+			}
+			return modules.get(ctr);
+
+		}
+		return null;
+
+	}
+	/* Deletes Module whose id is mid */
+	@DeleteMapping("/api/module/{mid}")
+	public void deleteModule(@PathVariable("mid") int mid, HttpSession session) {
+		List<Course> courses = courseService.findAllCourses(session);
+		Module reqdModule = null;
+		for (Course c : courses) {
+			modules = c.getModules();
+			for (Module m : modules) {
+				if (m.getId() == mid) {
+					reqdModule = m;
+					break;
+				}
+			}
+		}
+		if(reqdModule!=null)
+		{
+			
+			for(Course c: courses)
+			{
+				modules = c.getModules();
+				List<Module> resultModules = (List<Module>) modules.stream().filter(x -> x.getId() != mid)
+						.collect(Collectors.toList());
+				c.setModules(resultModules);
+			}
+		}
 	}
 }
